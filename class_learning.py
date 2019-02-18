8783b112143ad8ce78ae16df89aebdfcbb4358cd
Aliaz <aliazn2013@yahoo.com>
2019-02-18 23:02:46 +0330

    is_instance_function

--- class_learning.py
+++ class_learning.py
@@ -1,5 +1,9 @@
-# exercise to learn
+# isinstance
 print(isinstance('a', str))
 print(isinstance(1.2, (float, int)))
 print(isinstance(True, bool))
 print(isinstance(False, int))
+# subclass
+print(issubclass(int, float))
+print(issubclass(bool, int))
+
