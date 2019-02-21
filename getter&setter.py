d723d04f6e66b4727198d866873c065ef831e541
Aliaz <aliazn2013@yahoo.com>
2019-02-21 23:09:33 +0330

    exercise

--- getter&setter.py
+++ getter&setter.py
@@ -2,5 +2,17 @@ class Circle:
     def __init__(self, diameter):
         self.diameter = diameter
 
+    @property
     def radius(self):
         return self.diameter / 2
+
+    @radius.setter
+    def radius(self, radius):
+        self.diameter = radius *2
+
+
+small_one = Circle(10)
+small_one.radius = 20
+print(small_one.diameter)
+print(small_one.radius)
+print(small_one.diameter)
