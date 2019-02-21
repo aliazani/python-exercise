e491225b8886f5ded1e645f9ef6476518e92593c
Aliaz <aliazn2013@yahoo.com>
2019-02-21 21:59:15 +0330

    exercise

--- book_pj.py
+++ book_pj.py
@@ -6,4 +6,20 @@ class Book:
     def __str__(self):
         return f'{self.title} by {self.author}'
 
+
 class BookCase:
+    def __init__(self, books=None):
+        self.books = books
+
+    @classmethod
+    def create_books_case(cls, book_list):
+        book = []
+        for title, author in book_list:
+            book.append(Book(title, author))
+        return cls(book)
+
+
+b = BookCase.create_books_case([('machine', 'mh.ahmad'), ('string', 'mohammad')])
+print(b)
+print(b.books)
+print(str(b.books[0]))
