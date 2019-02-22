6794baf06ceef7abd93cf382685059a6e95281d6
aliazani <aliazn2013@yahoo.com>
2019-02-22 18:05:20 +0330

    class

---
+++ database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+def get_info(name, family_name, age, sex):
+    query = conn.execute(f''''INSERT INTO people(Name, Family_name, Age, Sex) VALUES('{name}', '{family_name}', {age}, '{sex}')
+    
+               ''')
+
+    conn.execute(query)
+
+
+conn = sqlite3.connect('exercise.db')
+conn.execute('''CREATE TABLE people(
+        ID PRIMARY KEY INTEGER AUTOINCREMENT,
+        Name    VARCHAR(45), 
+        Family_name   VARCHAR(45),
+        Age     INT,
+        Sex     VARCHAR(1)) ''')
