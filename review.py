1bbc490507dd84801b525281d2885386b2996df7
Aliaz <aliazn2013@yahoo.com>
2019-02-22 13:47:44 +0330

    exercise

--- review.py
+++ review.py
@@ -1,1 +1,21 @@
-class Shape:
+class Celsius:
+
+    def __get__(self, instance, owner):
+        return 5 * (instance.fahrenheit - 32) / 9
+
+    def __set__(self, instance, value):
+        instance.fahrenheit = 32 + 9 * value / 5
+
+
+class Temperature:
+
+    celsius = Celsius()
+
+    def __init__(self, initial_f):
+        self.fahrenheit = initial_f
+
+
+t = Temperature(212)
+print(t.celsius)
+t.celsius = 0
+print(t.fahrenheit)
