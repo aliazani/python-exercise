bf55171533e1b019c922312d62fa5903bb9c6f3f
aliazani <aliazn2013@yahoo.com>
2019-02-22 19:23:20 +0330

    class

---
+++ .idea/flask_course.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/login/<name>/<int:rep>')
+def login(name, rep):
+    return render_template('login.html', name=name, path="https://www.google.com", rep=rep)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
