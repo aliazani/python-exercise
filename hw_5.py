59fc5bc14306a635cc13000e916c64adccbef2cb
Aliaz <aliazn2013@yahoo.com>
2019-02-28 22:21:26 +0330

    new release

---
+++ hw_5.py
@@ -0,0 +1,6 @@
+class DB:
+    def __new__(cls):
+        pass
+
+    def __init__(self, *args, **kw):
+        pass
---
+++ pickele.py
@@ -0,0 +1,20 @@
+import pickle
+
+a = ['test value', 'test value 2', 'test value 3']
+file_Name = "testfile"
+# open the file for writing
+fileObject = open(file_Name, 'wb')
+
+# this writes the object a to the
+# file named 'testfile'
+pickle.dump(a, fileObject)
+
+# here we close the fileObject
+fileObject.close()
+# we open the file for reading
+fileObject = open(file_Name, 'rb')
+print()
+# load the object from the file into var b
+b = pickle.load(fileObject)
+
+print(a == b)
