057d37eca24eea4ee0783992abe4f7a8cd505ef1
Aliaz <aliazn2013@yahoo.com>
2019-03-10 23:50:03 +0330

    create table with out sql programing just with python

--- orm_2.py
+++ orm_2.py
@@ -1,5 +1,26 @@
-# from sqlalchemy import create_engine
-#
-# db_conn = 'postgresql+psycopg2://scott:tiger@localhost/mydatabase'
-# engine = create_engine(db_conn, echo=True)
-# engine.execute('select 1').scalar()
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, INTEGER
+
+db_conn = 'sqlite:///F:\\rez.db'
+engine = create_engine(db_conn, echo=True)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'people'
+    idi = Column(INTEGER, primary_key=True)
+    name = Column(String)
+    f_name = Column(String)
+    password = Column(String)
+
+    def __init__(self, name, f_name, password):
+        self.name = name
+        self.f_name = f_name
+        self.password = password
+
+    def __repr__(self):
+        return f"<User('{self.name}', '{self.f_name}', '{self.password}')>"
+
+
+Base.metadata.create_all(engine)
