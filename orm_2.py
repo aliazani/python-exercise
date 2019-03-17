22020bd0a7f1517c65e0e2047086c01ca55b19ca
Aliaz <aliazn2013@yahoo.com>
2019-03-17 23:14:43 +0330

    sql lite

--- orm_2.py
+++ orm_2.py
@@ -1,26 +1,34 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, INTEGER
+from sqlalchemy.orm import sessionmaker
 
 db_conn = 'sqlite:///F:\\rez.db'
 engine = create_engine(db_conn, echo=True)
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 class User(Base):
-    __tablename__ = 'some'
+    __tablename__ = 'People'
     idi = Column(INTEGER, primary_key=True)
     name = Column(String)
-    f_name = Column(String)
+    last_name = Column(String)
     password = Column(String)
 
-    def __init__(self, name, f_name, password):
+    def __init__(self, name, last_name, password):
         self.name = name
-        self.f_name = f_name
+        self.last_name = last_name
         self.password = password
 
     def __repr__(self):
-        return f"<User('{self.name}', '{self.f_name}', '{self.password}')>"
+        return f"<User('{self.name}', '{self.last_name}', '{self.password}')>"
 
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
+add_user = User("ali", "azani", "Aliazani1378")
+print(add_user.idi)
+print(add_user.name)
+session.add(add_user)
+session.commit()
